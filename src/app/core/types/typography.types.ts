e0a183a11c45586e36db0a9c67b2d28376f6a30c
Maxime1995 <maxime.noel19@gmail.com>
2026-08-23 08:48:07 +0200

    Creating core types

---
+++ src/app/core/types/typography.types.ts
@@ -0,0 +1,14 @@
+export type TypographyScale =
+    | 'display'
+    | '2xl'
+    | 'xl'
+    | 'lg'
+    | 'md'
+    | 'sm'
+    | 'xs';
+
+export type TypographyWeight =
+    | 'regular'
+    | 'medium'
+    | 'semibold'
+    | 'bold';
